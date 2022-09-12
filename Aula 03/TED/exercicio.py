ee68e4454c426f4ea09051a50dcf0797670805ae
Jayme Holanda <112284700+JaymeHolanda@users.noreply.github.com>
2022-09-12 17:45:46 -0300

    Create exercicio.py

---
+++ Aula 03/TED/exercicio.py
@@ -0,0 +1,32 @@
+
+#Ex.01
+#Crie um programa em Python que imprima uma mensagem que você ama programar com esta linguagem. ok 
+
+print('Eu amo programar em python!')
+
+#Ex.02
+#Crie um programa em Python que pergunte o nome do usuário e imprima um bom dia com o nome do usuário. Dica: você pode utilizar o método .format() ou uma concatenação de string, por exemplo
+
+nome = input ('Qual o seu nome meu jovem ?')
+print(f'Tenha um ótimo dia {nome}')
+
+#Ex.03
+#Crie um programa em Python que solicite um número do usuário, depois some este número com 1357, multiplique por 8, divida por 5 e eleve ao quadrado.
+
+n = float(input ('Digite um número aqui ...') )
+print((((n+1337)*8)/5)**2)
+
+#Ex.04 
+# Crie um programa em Python que imprima um convite para uma festa com o nome do usuário que for digitado. O nome do usuário precisará ser apresentado todo em caixa alta.
+
+nome = input('Coloque seu nome para o registro do convite aqui    ')
+print(f' {nome.upper()} você foi convidado para o casamento ! ')
+
+#Ex.05
+#Crie um programa em Python em que as quatro operações em que todos os resultados resultam em 8.
+
+print(16-8)
+print(4*2)
+print(16/2)
+print(4+4)
+
