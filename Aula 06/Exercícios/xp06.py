e1521f57a13cc4e76413a98f769877a201cc5451
Jayme Holanda <112284700+JaymeHolanda@users.noreply.github.com>
2022-09-13 20:13:51 -0300

    comit

--- Aula 06/Exercícios/xp06.py
+++ Aula 06/Exercícios/xp06.py
@@ -1,3 +1,33 @@
-#len ( começa do 0. Mostra a quantidade númerica de variantes dentro)
-#while( estrutura de repetição, devendo ser especificado)
-#for
+#len = começa do zero. Mostra a quantidade numerica dentro de uma variante
+#while (enquanto - faça(:)) = Faz a repetição. Pode ser no começo um número e depois uma palavra
+#dowhile (Garantir que pelo menos uma vez foi executado)
+#for (cicla dentro de uma lista, pecorrendo toda a lista.) 1 . 2. 3. 
+
+#x = int(input('Digite um valor para x   '))
+#while x < 30:
+ #   print(x)
+  #  x +=1
+
+# para x de 0 até 100 faça escrever (x) fim 
+
+#for num in range (30):
+ #   print('O número agora é:' +str(num))
+
+#frutas = ['Abacaxi', 'Morango', 'Uva']
+#for frutas in frutas:
+    #print(frutas)
+
+# abacate = 1
+
+#EXEMPLO DO WHILE
+
+#while abacate < 10:
+ #   print(abacate)
+  #  abacate += 1
+
+# EXEMPLO DO 
+
+#Escreva um algoritmo para imprimir os números de 1 (inclusive) a 10 (inclusive) em ordem crescente.
+
+for casa in range (1,11,1):
+    print(casa)
