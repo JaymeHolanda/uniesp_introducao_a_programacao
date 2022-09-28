f8a703e5bf968c143ae0f24d7c9b85fa9f059c25
Jayme Holanda <112284700+JaymeHolanda@users.noreply.github.com>
2022-09-27 21:38:47 -0300

    aula 08 - atividade avaliativa

--- Aula 06/Teds/ted06.py
+++ Aula 06/Teds/ted06.py
@@ -17,10 +17,37 @@
   #  if x == 'banana':
    #     break
 
+#lista = ['',''] /  
+# for nome in lista
+
+#Desenvolva um programa que verifique e mostre os números entre 1.000 e 2.000 que, quando
+#divididos por 11, produzam o resto igual a 5.
 
 
 
+cadastros = []
+botao = 1000
 
-        
+while botao !=0:
+  print('Digite 1 para cadastrar um novo usuário')
+  print('Digite 2 para imprimir todos os usuários')
+  print('Digite 0 para sair do programa de Cadastro')
 
- 
+  botao = int(input('Digite a opção desejada     '))
+
+  if botao == 1: 
+    nome = input("Qual o seu nome ?   ")
+    idade = input("Qual o seu idade ?   ")
+    email = input("Qual o seu email ?   ")
+  #Folha de Cadastros:
+    dados = [nome,idade,email]
+  #Guardando a folha na pasta
+    cadastros.append(dados) 
+  
+  elif botao == 2:
+    for x in cadastros: 
+      print(x)
+  elif botao == 0:
+    print("Obrigado por acessar este software!")
+  else:
+    print('Digite um número válido')
---
+++ Aula 08/exercicio08-Q1.py
@@ -0,0 +1,13 @@
+a = float(input("Qual o valor de A ?    "))
+b = float(input("Qual o valor de B ?    "))
+c = float(input("Qual o valor de C ?    "))
+
+d = b**2 - 4 * (a * c )
+
+x1 = (-b + (d**0.5) ) / (2*a)
+print (x1)
+
+x2 = (-b - (d**0.5) ) / (2*a)
+print (x2)
+
+
